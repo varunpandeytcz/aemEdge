c6d1b6f40836c5a8f3e69c119da6b501b5aa45fc
unknown <varun.pandey@techchefz.com>
2024-06-13 02:30:03 +0530

    New

--- blocks/testBlockCheck/testBlockCheck.js
+++ blocks/testBlockCheck/testBlockCheck.js
@@ -1,4 +1,4 @@
-export default function decorate(block){
+export default function decorate (block) { 
 
     [...block.children].forEach((row,r)=>{
         row.classList.add("rowContainer");
@@ -20,7 +20,6 @@ export default function decorate(block){
                     var ctaImg = div.querySelector("picture");
                     ctaImg.classList.add("ctaImg");
                 }
-
                 
             });
     });
