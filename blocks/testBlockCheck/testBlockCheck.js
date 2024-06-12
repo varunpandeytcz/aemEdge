5828d1ae72a402d88accc8948df79ab4a47106ae
unknown <varun.pandey@techchefz.com>
2024-06-13 03:09:20 +0530

    New 1

--- blocks/testBlockCheck/testBlockCheck.js
+++ blocks/testBlockCheck/testBlockCheck.js
@@ -1,19 +1,19 @@
 export default function decorate(block) {
   [...block.children].forEach((row) => {
-    row.classList.add('rowContainer');
+    row.classList.add('row-Container');
     [...row.children].forEach((div, d) => {
       if (d === 0) {
         div.classList.add('column1');
-        const ctaText = div.querySelector('p');
-        ctaText.classList.add('ctaText');
+        const cta-Text = div.querySelector('p');
+        cta-Text.classList.add('cta-Text');
         const innerText = ctaText.innerHTML;
         const regex = /\$([^$]+)\$/g;
         const editedText = innerText.replace(regex, '<span class="highlight">$1</span>');
         ctaText.innerHTML = editedText;
       } else if (d === 1) {
         div.classList.add('column2');
-        const ctaImg = div.querySelector('picture');
-        ctaImg.classList.add('ctaImg');
+        const cta-Img = div.querySelector('picture');
+        ctaImg.classList.add('cta-Img');
       }
     });
   });
