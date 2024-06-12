fbf428e9618c282e006bcbc605157e84983180af
unknown <varun.pandey@techchefz.com>
2024-06-13 02:57:32 +0530

    New 1

--- blocks/testBlockCheck/testBlockCheck.js
+++ blocks/testBlockCheck/testBlockCheck.js
@@ -1,21 +1,21 @@
 export default function decorate(block) {
-  [...block.children].forEach((row) => {
-    row.classList.add('rowContainer');
-    [...row.children].forEach((div, d) => {
-      if (d === 0) {
-        div.classList.add('column1');
-        const ctaText = div.querySelector('p');
-        ctaText.classList.add('ctaText');
-        const innerText = ctaText.innerHTML;
-        const regex = /\$([^$]+)\$/g;
-        const editedText = innerText.replace(regex, '<span class="highlight">$1</span>');
-        ctaText.innerHTML = editedText;
-      } else if (d === 1) {
-        div.classList.add('column2');
-        const ctaImg = div.querySelector('picture');
-        ctaImg.classList.add('ctaImg');
-      }
+    [...block.children].forEach((row) => {
+        row.classList.add('rowContainer');
+        [...row.children].forEach((div, d) => {
+            if (d === 0) {
+                div.classList.add('column1');
+                const ctaText = div.querySelector('p');
+                ctaText.classList.add('ctaText');
+                const innerText = ctaText.innerHTML;
+                const regex = /\$([^$]+)\$/g;
+                const editedText = innerText.replace(regex, '<span class="highlight">$1</span>');
+                ctaText.innerHTML = editedText;
+            } else if (d === 1) {
+                div.classList.add('column2');
+                const ctaImg = div.querySelector('picture');
+                ctaImg.classList.add('ctaImg');
+            }
 
+        });
     });
-  });
 }
