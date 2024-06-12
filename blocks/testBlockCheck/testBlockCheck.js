09854c5275052ad8fea0049df0ffdb9b1f47ecda
unknown <varun.pandey@techchefz.com>
2024-06-13 02:44:24 +0530

    New 1

--- blocks/testBlockCheck/testBlockCheck.js
+++ blocks/testBlockCheck/testBlockCheck.js
@@ -1,22 +1,22 @@
 export default function decorate(block) {
   [...block.children].forEach((row) => {
     row.classList.add('rowContainer');
-            [...row.children].forEach((div,d) => {
-                if(d==0){
-                    div.classList.add('column1');
-                    var ctaText = div.querySelector('p');
-                    ctaText.classList.add('ctaText');
-                    const innerText = ctaText.innerHTML;
-                    const regex = /\$([^$]+)\$/g;
-                    const editedText = innerText.replace(regex, '<span class="highlight">$1</span>');
-                    ctaText.innerHTML = editedText;
-                }
-                else if(d==1){
-                    div.classList.add('column2');
-                    var ctaImg = div.querySelector('picture');
-                    ctaImg.classList.add('ctaImg');
-                }
+    [...row.children].forEach((div, d) => {
+      if (d === 0){
+        div.classList.add('column1');
+        var ctaText = div.querySelector('p');
+        ctaText.classList.add('ctaText');
+        const innerText = ctaText.innerHTML;
+        const regex = /\$([^$]+)\$/g;
+        const editedText = innerText.replace(regex, '<span class="highlight">$1</span>');
+        ctaText.innerHTML = editedText;
+      }
+      else if(d==1){
+        div.classList.add('column2');
+        var ctaImg = div.querySelector('picture');
+        ctaImg.classList.add('ctaImg');
+      }
 
-            });
     });
+  });
 }
