caff1ef655ed5b1bbb38188ce1222eebfe8a1239
unknown <varun.pandey@techchefz.com>
2024-07-30 12:54:20 +0530

    Merging all component

--- blocks/sdcomponent/sdcomponent.js
+++ blocks/sdcomponent/sdcomponent.js
@@ -9,7 +9,6 @@ export default function decorate(block) {
         if (d === 1) {
           div.classList.add('second-div');
           const tdElement = div.querySelectorAll('td');
-          console.log(tdElement);
           tdElement.forEach((td, index) => {
             td.classList.add(`card${index + 1}`);
           });
--- blocks/topcomponent/topcomponent.js
+++ blocks/topcomponent/topcomponent.js
@@ -1,21 +1,21 @@
 export default function decorate(block) {
-    [...block.children].forEach((row, r) => {
-      if (r === 0) {
-        row.classList.add("outer");
-        [...row.children].forEach((div, d) => {
-          if (d === 0) {
-            div.classList.add("first-cntnr");
-          }
-          if (d === 1) {
-            div.classList.add("second-cntnr");
-          }
-          if (d === 2) {
-            div.classList.add("third-cntnr");
-          }
-          if (d === 3) {
-            div.classList.add("fourth-cntnr");
-          }
-        });
-      }
-    });
-  }
+  [...block.children].forEach((row, r) => {
+    if (r === 0) {
+      row.classList.add('outer');
+      [...row.children].forEach((div, d) => {
+        if (d === 0) {
+          div.classList.add('first-cntnr');
+        }
+        if (d === 1) {
+          div.classList.add('second-cntnr');
+        }
+        if (d === 2) {
+          div.classList.add('third-cntnr');
+        }
+        if (d === 3) {
+          div.classList.add('fourth-cntnr');
+        }
+      });
+    }
+  });
+}
