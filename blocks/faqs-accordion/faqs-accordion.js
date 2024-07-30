ab08e68eafdfe6339dc35f1e0a1943e3d91bbb31
unknown <varun.pandey@techchefz.com>
2024-07-30 12:12:03 +0530

    Merging all component

--- blocks/faqs-accordion/faqs-accordion.js
+++ blocks/faqs-accordion/faqs-accordion.js
@@ -1,33 +1,25 @@
 const handlefaqClick = (accordion) => {
-    const answer = accordion.currentTarget.nextElementSibling;
-    if (answer.classList.contains("show")) {
-        answer.classList.remove("show")
-        accordion.currentTarget.classList.remove("show")
+  const answer = accordion.currentTarget.nextElementSibling;
+    if (answer.classList.contains('show')) {
+      answer.classList.remove('show');
+      accordion.currentTarget.classList.remove('show');
     } else {
-        answer.classList.add("show")
-        accordion.currentTarget.classList.add("show")
+      answer.classList.add('show');
+      accordion.currentTarget.classList.add('show');
     }
-
-
 }
 
 export default function decorate(block) {
-
-    [...block.children].forEach((row, rowIndex) => {
-        row.className = "faqs-accordion-item";
-
-        [...row.children].forEach((faq, faqIndex) => {
-            if (faqIndex === 0) {
-                faq.className = "faqs-accordion-question";
-                faq.addEventListener("click", handlefaqClick);
-            }
-
-            if (faqIndex === 1) {
-                faq.className = "faqs-accordion-answer";
-            }
-        })
-
-
-
-    });
+  [...block.children].forEach((row, rowIndex) => {
+    row.className = 'faqs-accordion-item';
+      [...row.children].forEach((faq, faqIndex) => {
+        if (faqIndex === 0) {
+          faq.className = 'faqs-accordion-question';
+          faq.addEventListener('click', handlefaqClick);
+        }
+        if (faqIndex === 1) {
+          faq.className = 'faqs-accordion-answer';
+        }
+      })
+  });
 }
